693108b5948db9d46bde5457dc2a1e1d03c177e4
Denny Huang <denny0223@gmail.com>
2019-10-30 11:53:13 +0800

    [Content] Remove event intro

--- src/pages/CFP.vue
+++ src/pages/CFP.vue
@@ -3,11 +3,6 @@
     <Banner/>
     <main class="spotlight-container">
       <div class="spotlight">
-        <h1 class="font-black">活動介紹</h1>
-        <p>在台灣，不只開源文化需要推廣，台語文化也需要推廣與傳承，如果日常生活不常使用台語，我們也將失去以台語溝通解釋的能力。</p>
-        <p>COSCUP 2019 來台講是一場「來用台語講開源」的專場活動，在過去 COSCUP 的研討會中，雖曾有幾位在地的社群朋友以台語進行 Open Source 技術演講，但大家還是普遍選擇以華文或英文為主，為此 COSCUP 將選在台北以外的地區，舉辦 COSCUP 2019 來台講，目的是希望讓大家有機會以台語進行技術交流，活絡台灣各地的開源社群聚會。</p>
-      </div>
-      <div class="spotlight">
         <h1 class="font-black">贊助資訊</h1>
         <p>COSCUP 是由全志工籌辦舉辦的活動，活動也是開放大眾免費報名，我們誠摯地邀請台中在地或支持開源的企業組織成為本專場贊助商。</p>
         <p>歡迎來信洽詢 <a href="mailto:sponsorship@coscup.org" class="font-black">sponsorship@coscup.org</a></p>
