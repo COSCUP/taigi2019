ce08a508c324318ac499780da985a5a15fd5eeff
Singing Li <snoopy60313@gmail.com>
2019-12-04 09:48:01 +0800

    remove call for paper

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -8,12 +8,6 @@
       />
     </div>
     <header class="banner-header">
-      <h3
-        v-if="$route.name === 'CFP'"
-        class="flag"
-      >
-        Call for Paper｜徵稿開催
-      </h3>
       <h1 class="title bold">
         <span>COSCUP 2019 </span>
         <span v-html="parser('來/Lâi/台/Tâi/講/Káng/')"/>
@@ -26,12 +20,6 @@
         <span class="date">Dec. 21, 2019</span>
         <span class="place"><font-awesome-icon :icon="['fas', 'map-marker-alt']"/><a href="https://tools.wmflabs.org/geohack/geohack.php?params=24_9_8_N_120_41_0_E" target="_blank" rel="noopener noreferrer">　臺中科技大學</a></span>
       </p>
-      <h2
-        v-if="$route.name === 'CFP'"
-        class="button"
-      >
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfytjV4lqx06KUnLOdP0ejXV0mfrrfWRK68ESiHEUGSLZbGXw/viewform" target="_blank" rel="noopener noreferrer" v-html="parser('我/Góa/欲/Beh/投/Tâu/稿/Kó/')"></a>
-      </h2>
     </header>
   </div>
 </template>
