9509043f2c30fa02e160adf8323860e28eea0dff
Po-chiang Chao <bobchao@gmail.com>
2019-12-07 01:39:25 +0800

    Add programme and hoagi

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,28 +4,129 @@
     <main class="spotlight-container">
       <div class="spotlight">
         <h1 class="font-black">世界第一擺ê台語專場技術研討會，等你做夥來</h1>
-        <p>咱全台灣有 80% ê人講台語，結果soah英語ê研討會比台語--ê較chē？你講你會曉講，按呢你kám想過beh按怎用台語解說電腦ê物仔？</p>
+        <p>咱全台灣有 70% ê人講台語，結果soah英語ê研討會比台語--ê較chē？你講你會曉講，按呢你kám想過beh按怎用台語解說電腦ê物仔？</p>
         <p>來來來，這著有一ê機會hōo--lí試看覓：COSCUP「來台講」，世界頭一擺ê台語專場技術研討會！</p>
+
+        <section class="hoagi">
+        <p>全台灣有 70% 的人會講台語，但連純英語的研討會都比以台語為主的研討會多？你說你會講，那你有想過怎麼用台語說明電腦的東西嗎？</p>
+        <p>來來來，這裡有個機會讓你試試看：COSCUP「來台講」，世界第一次舉辦的台語專場技術研討會！</p>
+        </section>
+
       </div>
-      <div class="spotlight">
+      <div class="spotlight" id="programme">
         <h1 class="font-black">開源囡仔，做夥舞！</h1>
         <p>「台語開源研討會」hiah好耍（可能mā會足好聽）ê想法，咱這款ta̍k項攏愛家己舞ê開源人是當然愛做夥來插一kha，這má就來kā好料理款款--ê排上桌！</p>
-        <ul class="bullets">
-          <li>看 Wikidata 寫臺文ê 王文岳紹介台語ē多種姿態</li>
-          <li>你咁有用 Line？ 林家煜有一tsiah完全用 kamigo 做出來ê muscle-man line bot</li>
-          <li>Tēⁿ Hiàn Kun 做ê開源義手mā看會到，這枝手有目睭</li>
-          <li>會曉念bē曉寫？吳家銘ê Chhōe台語來Phahkā你tàu-saⁿ-kāng</li>
-          <li>著想要有台語文ê地圖？ 陳瑞霖講用 OpenStreetMap 做台語、客家話ê地圖嘛會使</li>
-          <li>手機仔唔夠 open？kā freedom 做夥用 open-source 工具來探看走 iOS ê物仔內底kah溫度相關ê軟體kah硬體</li>
-          <li>電腦要怎樣講台語？意傳科技來紹介全全開源ê語音合成──媠聲SuíSiann</li>
-        </ul>
+        
+        <section class="hoagi">
+        <p>「台語開源研討會」這麼好玩（可能也會很好聽）的想法，像我們這種老愛自幹的開源人當然都要來參一腳！這就來把好菜端上桌：</p>
+        </section>
+
+        <section id="sessions">
+<section class="session">
+    <p class="time">09:30-10:10</p>
+    <section class="talk">
+        <h1>Wikidata 寫臺文</h1>
+        <p>Wikidata 是維基下的一個子計畫，嘛是全球尚大的跨語言結構化資料庫。目前已經會用收錄百幾種的語言，其中當然包含有臺語。佇 Wikidata 中，臺語文需要安怎收錄，無仝款的輸入法相衝的時陣又欲安怎處理？擱來佇咧小眾語言擱會當如何用 Wikidata 來進行語言的保存佮紀錄，這改就講予你知。</p>
+    </section>
+    <section class="speaker">
+        <h2>王文岳</h2>
+        <p>李梅樹紀念館資訊組召集人，Wikidata Taiwan 社群共同主持人</p>
+    </section>
+</section>
+    
+<section class="session">
+    <p class="time">10:20-11:00</p>
+    <section class="talk">
+        <h1>完全用 kamigo 做出來ê muscle-man line bot</h1>
+        <p>寫 line bot ê工具，逐擺攏是 copy 進前ê project 來走。「卡米狗」 ê老爸做出來這leh ho- 逐家用ê framework - Kamigo，這擺ê line bot 著是全全用這來做ê，tī遮會kā逐家講會安怎用，而且是安怎幫助我用 Ruby 來寫出我家己紀錄練身體ê line bot。</p>
+    </section>
+    <section class="speaker">
+        <h2>林家煜</h2>
+        <p>NiJia 是一ê tī台中浪流連 26 年ê少年仔，平常時無聊著愛寫「line bot」來耍，著因為按呢煞無小心變成 charbot Taiwan 管理員之一，最近tú變成 LINE API Expert，歡迎逐家來討論抑是beh開講我攏真歡迎！</p>
+    </section>
+</section>
+    
+<section class="session">
+    <p class="time">11:10-11:50</p>
+    <section class="talk">
+        <h1>Chit ki chhiú ū ba̍kchiu (Hands with eyes)</h1>
+        <p>TGH (Taiwan Gods Hand project, 台灣神手 開源義肢) 開發 chiâⁿ 多開源 ê 人工肢体、目的是爲着卜乎有需要 ê 人會凍做伙舞、鬥陣學。TGH 人工肢体 ê 技術不時 teh 進步。眼前上新 ê 人工手號做 TGHiii AICGS。合着 AI 物体探測 ê 功能、乎未來 ê 人工手 ē 曉認 bat 物件、通成做閣卡利便 ê kesi。</p>
+    </section>
+    <section class="speaker">
+        <h2>Tēⁿ Hiàn Kun</h2>
+        <p>本身是工程師、專長是電腦圖影處理 (computer vision, image processing) kap 一卦機器學習 (machine learning)。有一 ê 蓋久無 teh 顧 ê YouTube 網路台: http://bit.ly/2vFI7je 。上 chia̍p 用 ê OS 是 Lubuntu/Ubuntu (自 6.04 用到今)、上 chia̍p 寫 ê 電腦程序語言是 Phaisiâ (Python) kap Bash。大部分 khangkhòe 朗是倚靠 Open Source ê 軟体 teh 處理。那無 Vim góa ē 死.</p>
+    </section>
+</section>
+    
+<section class="session">
+    <p class="time">13:00-13:40</p>
+    <section class="talk">
+        <h1>Chhōe台語來Phah</h1>
+        <p>紹介ChhoeTaigi台語辭典網站、app，和手機仔PhahTaigi台語輸入法ê開放專案現況kap未來規劃。</p>
+    </section>
+    <section class="speaker">
+        <h2>Ngô͘ Ka-bêng 吳家銘</h2>
+        <p>Tâi-gí Sóaⁿ-kang kiam Thêng-sek Choh-sit-lâng. （台語散工兼程式作穡人）</p>
+    </section>
+</section>
+    
+<section class="session">
+    <p class="time">13:50-14:30</p>
+    <section class="talk">
+        <h1>台語、客家話ê地圖嘛會使──OpenStreetMap ê濟語言才調</h1>
+        <p>台語、客家話甚至是原住民話標示地號名的地圖，敢有可能？本土化意識那來那旺，以前予人看袂起的本土語言有另外一個發展的門路，靠網路社群共編共享綴著時代的跤步。OpenStreetMap佮商業地圖無仝，靠眾人會當標示濟語言，甚至是標示在地族群的地號名。倚靠友好計畫Wikidata予人的幫助，OpenStreetMap會當加上足濟本土語言的地名號，甚至是會當標示國外的地號名，變成是面熟的語言。這擺的講演我會講OpenStreetMap的濟語言的架構，按怎當作本土語言倡義者的好家私，猶閣有目前台灣標示台語、客家話、原住民語的標示情勢。</p>
+    </section>
+    <section class="speaker">
+        <h2>陳瑞霖</h2>
+        <p>台灣維基協會成員，參與Wikidata台灣和OpenStreetMap台灣社群。逐個月舉行社群聚會，紹介和推廣OpenStreetMap和Wikidata。最近佇增加頭殼內底的台語詞彙，猶未熟似台羅和台語輸入法，是台語的青盲牛。</p>
+    </section>
+</section>
+    
+<section class="session">
+    <p class="time">14:40-15:20</p>
+    <section class="talk">
+        <h1>用 open-source 工具來探看走 iOS ê物仔內底kah溫度相關ê軟體kah硬體</h1>
+        <p>真濟朋友攏佮意用 iPhone 佮其他走 iOS 个物仔, 雖然 iOS 本身無完全 open source, 但是藉著 open source 个部分, 咱已經會使做足濟 Apple 無共你講个代誌。這个演講會介紹如何使用 open source 工具佮我家己寫个一寡 code 來揣出走 iOS 个物仔頂頭佮溫度相關个軟體佮硬體。因為欲瞭解其中个運作原理咱必需愛先有一寡 Objective-C 佮系統軟體相關知識, 所以這毋是入門个演講。</p>
+    </section>
+    <section class="speaker">
+        <h2>Tân Koan-Sîn</h2>
+        <p>I am a long-time open source user. I learned to use open source software before the term open source was coined on a VAX-11/780 running 4.3BSD. For the past couple year, I usually give talks in Taiwanese in COSCUP.</p>
+    </section>
+</section>
+    
+<section class="session">
+    <p class="time">15:30-16:10</p>
+    <section class="talk">
+        <h1>全全開源ê語音合成──媠聲SuíSiann</h1>
+        <p>SuíSiann是意傳科技ê開源語音合成計畫，希望ē-tàng予咱台灣語言ê發展閣較緊，自台灣話起頭，到客話、族語，予台灣ǹg語言平權行！</p>
+
+        <p>合成是kā文字變成聲音ê技術，比起傳統錄音方式koh較khin-khó，予語音應用tsiânn多元。</p>
+
+        <p>意傳參考LJ Speech ê概念，準備台灣話語料庫，kah模型訓練ê script公開，予APP開發、合成技術研究、文化保存團體，ē-sái分工注心做ka-tī專長ê代誌。</p>
+
+        <p>Demo Bāng-tsí: https://suisiann.ithuan.tw/</p>
+
+    </section>
+    <section class="speaker">
+        <h2>Ì-thuân Kho-ki Iú-hān Kong-si 意傳科技有限公司</h2>
+        <p>意傳科技是台語文專業ê資訊公司，阮ê目標是用科技推sak台語，向望透過阮ê技術kah台語文專長，hōo台語ê影音製作kah生活應用koh khah利便。</p>
+
+<p>現此時ê服務有台語字幕製作、台語口白文稿翻譯，嘛有台語輸入法、語音合成、語料庫等等資訊技術。</p>
+</p>
+    </section>
+</section>
+</section>
+
         <p>不管你是台語輾轉到會當主持婚宴ê老司機，抑是勉強聽有m̄-koh一句講都bē曉話ê菜鳥仔，攏歡迎來參加這場有夠純koh豐沛ê台語盛會，看咱ê Taiwanese 和 Open Source kap-kap做夥會當變出啥物báng！</p>
+        <section class="hoagi">
+        <p>不管你是台語溜到能唱 Rap 的老司機，還是免強聽懂但講不太出來的菜鳥，都歡迎來參加這場台語盛會，看 Taiwanese 跟 Open Source 跟的混搭能激盪出什麼火花！</p>
+        </section>
       </div>
       <div class="spotlight">
         <h1 class="font-black">活動資訊</h1>
         <ul class="bullets">
           <li>時間：2019/12/21（六） 09:30 - 17:00</li>
-            <li>地點：國立臺中科技大學 中技大樓 2F 多媒體會議室<br>（404 台中市北區錦平街 40 號，<a href="https://www.nutc.edu.tw/files/11-1000-150.php">交通資訊</a>、<a href="https://tools.wmflabs.org/geohack/geohack.php?params=24_9_8_N_120_41_1_E">地點座標 / GeoLocation</a>）</li>
+            <li>地點：國立臺中科技大學 中技大樓 2F 多媒體會議室 （404 台中市北區錦平街 40 號，<a href="https://www.nutc.edu.tw/files/11-1000-150.php">交通資訊</a>、<a href="https://tools.wmflabs.org/geohack/geohack.php?params=24_9_8_N_120_41_1_E">地點座標 / GeoLocation</a>）</li>
           <li>報名時間：2019/12/04 12:00 (+0800)</li>
         </ul>
       </div>
